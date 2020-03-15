3c5e2b6a54ecfe637270d692f36246a15f440306
Edd Sansome <edd@murismo.com>
2020-03-15 14:36:08 +0000

    Updates

---
+++ precache-manifest.87939865455b73c45cae0ca637c889d0.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdaf60b1cb01c70dce30e863da2f3c41",
+    "url": "/burger-bar-react/index.html"
+  },
+  {
+    "revision": "ac0adc23de18a6767e20",
+    "url": "/burger-bar-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9487dd968adcf00f7a58",
+    "url": "/burger-bar-react/static/css/main.c003ed5a.chunk.css"
+  },
+  {
+    "revision": "ac0adc23de18a6767e20",
+    "url": "/burger-bar-react/static/js/2.e274a55d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/burger-bar-react/static/js/2.e274a55d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9487dd968adcf00f7a58",
+    "url": "/burger-bar-react/static/js/main.7fbc7aad.chunk.js"
+  },
+  {
+    "revision": "c675469094dfc68bc688",
+    "url": "/burger-bar-react/static/js/runtime-main.2dff2aa9.js"
+  },
+  {
+    "revision": "032907bf16d67bd40163e581270f1942",
+    "url": "/burger-bar-react/static/media/bocker.032907bf.jpg"
+  },
+  {
+    "revision": "99274d7c04fd6ca0667818ef162c601b",
+    "url": "/burger-bar-react/static/media/bud.99274d7c.jpg"
+  },
+  {
+    "revision": "409771447c807b0130fae860d6720c26",
+    "url": "/burger-bar-react/static/media/burger1.40977144.jpg"
+  },
+  {
+    "revision": "be460bb3060003ac4e54eccde1bcca23",
+    "url": "/burger-bar-react/static/media/burger1.be460bb3.jpg"
+  },
+  {
+    "revision": "2efb49065c89e35db3197edfe3eb4154",
+    "url": "/burger-bar-react/static/media/burger10.2efb4906.jpg"
+  },
+  {
+    "revision": "1d6cce8e25de6cb3bb9da3c59f478f3f",
+    "url": "/burger-bar-react/static/media/burger2.1d6cce8e.jpg"
+  },
+  {
+    "revision": "730049959415eae0a1d2e526a6eba562",
+    "url": "/burger-bar-react/static/media/burger2.73004995.jpg"
+  },
+  {
+    "revision": "3b298361bb2da5e290e9ec8330a85012",
+    "url": "/burger-bar-react/static/media/burger3.3b298361.jpg"
+  },
+  {
+    "revision": "d2bd12a0ed443c9cffb9d4f3eedf7c59",
+    "url": "/burger-bar-react/static/media/burger5.d2bd12a0.jpg"
+  },
+  {
+    "revision": "9e7e1f6ce7e7f7064d540fc67839e7b8",
+    "url": "/burger-bar-react/static/media/burger6.9e7e1f6c.jpg"
+  },
+  {
+    "revision": "0e5a05efb9da9cbaab6e4e42210ef595",
+    "url": "/burger-bar-react/static/media/burger7.0e5a05ef.jpg"
+  },
+  {
+    "revision": "e8139ede865089a74409ff93b3b91327",
+    "url": "/burger-bar-react/static/media/burger8.e8139ede.jpg"
+  },
+  {
+    "revision": "df25a76abdd98f983b0bb2b496001853",
+    "url": "/burger-bar-react/static/media/burger9.df25a76a.jpg"
+  },
+  {
+    "revision": "9152b71ca6221e354b48b6e557cdffd0",
+    "url": "/burger-bar-react/static/media/burgerlogo copy.9152b71c.png"
+  },
+  {
+    "revision": "a93aedecbf69a98ac25ffc9e33dea61a",
+    "url": "/burger-bar-react/static/media/chicken_wings.a93aedec.jpeg"
+  },
+  {
+    "revision": "b9ba2b4cdfe94404a4ab7b95991cb333",
+    "url": "/burger-bar-react/static/media/chickstrips.b9ba2b4c.jpg"
+  },
+  {
+    "revision": "0272ef4cae641951595fb922ceb8c19c",
+    "url": "/burger-bar-react/static/media/choccake.0272ef4c.jpg"
+  },
+  {
+    "revision": "dca303c2335881bd8468e16fa5873cb8",
+    "url": "/burger-bar-react/static/media/coors.dca303c2.jpg"
+  },
+  {
+    "revision": "8d2e9eae7265acef511e49bc0daf477a",
+    "url": "/burger-bar-react/static/media/eton.8d2e9eae.jpg"
+  },
+  {
+    "revision": "05125e8f7c8950f4ecd9899a4103debc",
+    "url": "/burger-bar-react/static/media/gin.05125e8f.jpg"
+  },
+  {
+    "revision": "de1320cdbcce4a625958259170e14d21",
+    "url": "/burger-bar-react/static/media/mudpie.de1320cd.jpg"
+  },
+  {
+    "revision": "d7d5e6f3caa80fdff2038c6bc7c8a588",
+    "url": "/burger-bar-react/static/media/nachos.d7d5e6f3.jpg"
+  },
+  {
+    "revision": "2cdbc33f3aaedebe33602fc0e9c488f6",
+    "url": "/burger-bar-react/static/media/onion_rings.2cdbc33f.jpg"
+  },
+  {
+    "revision": "29f805e3833c6d2e2b895ec1d08f9a3e",
+    "url": "/burger-bar-react/static/media/vodka.29f805e3.jpeg"
+  },
+  {
+    "revision": "bb653272681775851139846485bd5ea4",
+    "url": "/burger-bar-react/static/media/wedges.bb653272.jpg"
+  }
+]);
